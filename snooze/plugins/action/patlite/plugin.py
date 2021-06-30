246b9cd2509fa4982e04a58c7a5db34e23ca98cd
Guillaume Ludinard <guillaume.ludi@gmail.com>
2021-06-30 20:40:13 +0900

    Support for Patlite as an action

---
+++ snooze/plugins/action/patlite/plugin.py
@@ -0,0 +1,29 @@
+'''Action plugin to send alerts to a Patlite'''
+
+from snooze.utils.patlite import Patlite as PatliteAPI, State
+from snooze.plugins.action import Action
+
+class Patlite(Action):
+    def __init__(self, core):
+        super().__init__(core)
+    def pprint(self, options):
+        '''
+        Determine the pretty print for the Patlite action plugin.
+        This is how the information will be printed on the web interface
+        to represent this action.
+        '''
+        host = options.get('host')
+        port = options.get('port')
+        lights = {k: v for k,v in options.get('lights').items() if v != 'off'}
+        output = "@{}:{} {}".format(host, port, lights)
+        return output
+    def send(self, record, options):
+        '''
+        Determine the action that will be taken when this action is invoked.
+        It will set the lights and alarm of the Patlite.
+        '''
+        lights = options.get('lights')
+        host = options.get('host')
+        port = int(options.get('port'))
+        with PatliteAPI(host, port=port) as patlite:
+            patlite.set_full_state(State(**lights))
---
+++ web/src/components/form/Patlite.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <b-form inline>
+    <b-input-group prepend="Red">
+        <b-form-select v-model="datavalue.red" :options="light_options"></b-form-select>
+    </b-input-group>
+    <b-input-group prepend="Yellow">
+        <b-form-select v-model="datavalue.yellow" :options="light_options"></b-form-select>
+    </b-input-group>
+    <b-input-group prepend="Green">
+        <b-form-select v-model="datavalue.green" :options="light_options"></b-form-select>
+    </b-input-group>
+    <b-input-group prepend="Blue">
+        <b-form-select v-model="datavalue.blue" :options="light_options"></b-form-select>
+    </b-input-group>
+    <b-input-group prepend="White">
+        <b-form-select v-model="datavalue.white" :options="light_options"></b-form-select>
+    </b-input-group>
+    </b-form>
+    {{ datavalue }}
+  </div>
+</template>
+
+<script>
+
+import Base from './Base.vue'
+
+var default_patlite = {red: 'off', yellow: 'off', green: 'off', blue: 'off', white: 'off'}
+
+export default {
+  extends: Base,
+  name: 'Patlite',
+  props: {
+    value: {type: Object, default: () => Object.assign({}, default_patlite) },
+    options: {type: Object},
+  },
+  data() {
+    return {
+      light_options: ['off', 'on', 'blink1', 'blink2'],
+      datavalue: this.value,
+    }
+  },
+  watch: {
+    datavalue: {
+      handler(v) { this.$emit('input', this.datavalue) },
+      deep: true,
+    },
+  },
+}
+
+</script>
