5ead8a989c4e61e2331851d7c2647d61df07d218
Guillaume Ludinard <guillaume.ludi@gmail.com>
2021-08-18 20:28:15 +0900

    Parser for the query language

---
+++ snooze/utils/parser.py
@@ -0,0 +1,130 @@
+'''Combinatory parser for the query language'''
+
+import pyparsing as pp
+from pyparsing import pyparsing_common as ppc
+
+pp.ParserElement.enablePackrat()
+
+EQUAL = pp.Literal('=')
+NOT_EQUAL = pp.Literal('!=')
+GT = pp.Literal('>')
+LT = pp.Literal('<')
+MATCHES = (pp.CaselessKeyword('MATCHES') | pp.Literal('~')).setParseAction(lambda: 'MATCHES')
+EXISTS = (pp.CaselessKeyword('EXISTS') | pp.Literal('?')).setParseAction(lambda: 'EXISTS')
+CONTAINS = (pp.CaselessKeyword('CONTAINS')).setParseAction(lambda: 'CONTAINS')
+IN = (pp.CaselessKeyword('IN')).setParseAction(lambda: 'IN')
+
+LPAR, RPAR, LBRACK, RBRACK, LBRACE, RBRACE, COLON = map(pp.Suppress, '()[]{}:')
+
+NOT = (pp.CaselessKeyword('NOT') | '!').setParseAction(lambda: 'NOT')
+OR = (pp.CaselessKeyword('OR') | '|').setParseAction(lambda: 'OR')
+AND = pp.Optional(pp.CaselessKeyword('AND') | '&').setParseAction(lambda: 'AND')
+
+valid_word = pp.Regex(r'[a-zA-Z0-9_.-]+')
+
+string = pp.QuotedString('"') | pp.QuotedString("'")
+boolean = (
+    pp.CaselessKeyword('true').setParseAction(lambda: True)
+    | pp.CaselessKeyword('false').setParseAction(lambda: False)
+)
+
+literal = pp.Forward()
+
+array_elements = pp.delimitedList(literal, delim=',')
+array = pp.Group(LBRACK + pp.Optional(array_elements, []) + RBRACK)
+array.setParseAction(lambda t: t.asList())
+hashmap = pp.Forward()
+
+fieldname = string | valid_word
+literal << (ppc.real | ppc.signed_integer | string | array | hashmap | boolean | valid_word)
+
+hashmap_element = pp.Group(fieldname + COLON + literal)
+hashmap_elements = pp.delimitedList(hashmap_element, delim=',')
+hashmap << pp.Dict(LBRACE + pp.Optional(hashmap_elements) + RBRACE)
+hashmap.setParseAction(lambda t: t.asDict())
+
+term = pp.Forward()
+expression = pp.Forward()
+
+operation = EQUAL | NOT_EQUAL | MATCHES | GT | LT | CONTAINS
+field_operation = EXISTS
+reverse_op = IN
+
+term << (
+    (fieldname('field') + operation('operation') + literal('value'))
+    | (fieldname('field') + field_operation('operation'))
+    | literal('field') + reverse_op('operation') + fieldname('value')
+    | literal('value')
+)
+
+class Term:
+    def __init__(self, tokens):
+        if 'field' in tokens:
+            self.field = tokens['field']
+            self.operation = tokens['operation']
+        else:
+            self.field = None
+            self.operation = None
+        if 'value' in tokens:
+            self.value = tokens['value']
+        else:
+            self.value = None
+    def __repr__(self):
+        if self.field and self.value:
+            return "Term({}, {})".format(self.operation, [self.field, self.value])
+        else:
+            return "Term({})".format(self.value)
+    def asList(self):
+        if self.field is not None and self.value is not None:
+            return [self.operation, self.field, self.value]
+        elif self.field is not None:
+            return [self.operation, self.field]
+        elif self.value is not None:
+            return ['SEARCH', self.value]
+
+class Operation:
+    def __init__(self, tokens):
+        tokens = tokens[0]
+        if len(tokens) > 1:
+            if tokens[0] == 'NOT':
+                self.op = 'NOT'
+                if tokens[1] == 'NOT':
+                    self.args = [Operation([tokens[1:]])]
+                else:
+                    self.args = [tokens[1]]
+            elif tokens[1] in ['AND', 'OR']:
+                self.op = tokens[1]
+                if len(tokens) > 3:
+                    self.args = [tokens[0], Operation([tokens[2:]])]
+                else:
+                    self.args = [tokens[0], tokens[2]]
+        else:
+            raise Exception("Unexpected operation: {}".format(tokens))
+    def __repr__(self):
+        return "Operation({}, {})".format(self.op, self.args)
+    def asList(self):
+        args = []
+        for arg in self.args:
+            if isinstance(arg, Operation):
+                args.append(arg.asList())
+            elif isinstance(arg, Term):
+                args.append(arg.asList())
+            else:
+                args.append(arg)
+        return [self.op, *args]
+
+term.setParseAction(Term)
+
+# Parse expressions that have an order of priority in operations
+expression << pp.infixNotation(
+    term,
+    [
+        (NOT, 1, pp.opAssoc.RIGHT, Operation),
+        (OR, 2, pp.opAssoc.LEFT, Operation),
+        (AND, 2, pp.opAssoc.LEFT, Operation),
+    ],
+)
+
+def parser(data):
+    result = expression.parseString(data).asList()[0].asList()
+    return result
---
+++ tests/utils/test_parser.py
@@ -0,0 +1,179 @@
+from snooze.utils.parser import parser
+
+class TestParserLogic:
+    def test_word(self):
+        result = parser('hello')
+        assert result == ['SEARCH', 'hello']
+
+    def test_key_value(self):
+        result = parser('key = value')
+        assert result == ['=', 'key', 'value']
+
+    def test_and(self):
+        result = parser('key1=value1 AND key2=value2')
+        assert result == ['AND', ['=', 'key1', 'value1'], ['=', 'key2', 'value2']]
+
+    def test_and_symbol(self):
+        result = parser('key1=value1&key2=value2')
+        assert result == ['AND', ['=', 'key1', 'value1'], ['=', 'key2', 'value2']]
+
+    def test_implicit_and(self):
+        result = parser('key1=value1 key2=value2')
+        assert result == ['AND', ['=', 'key1', 'value1'], ['=', 'key2', 'value2']]
+
+    def test_or(self):
+        result = parser('key1=value1 OR key2=value2')
+        assert result == ['OR', ['=', 'key1', 'value1'], ['=', 'key2', 'value2']]
+
+    def test_or_symbol(self):
+        result = parser('key1=value1|key2=value2')
+        assert result == ['OR', ['=', 'key1', 'value1'], ['=', 'key2', 'value2']]
+
+    def test_not(self):
+        result = parser('not key1=value1')
+        assert result == ['NOT', ['=', 'key1', 'value1']]
+
+    def test_not_symbol(self):
+        result = parser('!key1=value1')
+        assert result == ['NOT', ['=', 'key1', 'value1']]
+
+    def test_parenthesis(self):
+        result = parser('NOT (key1=value1 AND key2=value2)')
+        assert result == ['NOT', ['AND', ['=', 'key1', 'value1'], ['=', 'key2', 'value2']]]
+
+    def test_priority(self):
+        result = parser('NOT key1=value1 AND key2=value2')
+        assert result == ['AND', ['NOT', ['=', 'key1', 'value1']], ['=', 'key2', 'value2']]
+
+    def test_complex_query(self):
+        result = parser('myapp and source=syslog and custom_field = "myapp01" or custom_field = "myapp02"')
+        assert result == ['AND',
+            ['SEARCH', 'myapp'],
+            ['AND',
+                ['=', 'source', 'syslog'],
+                ['OR',
+                    ['=', 'custom_field', 'myapp01'],
+                    ['=', 'custom_field', 'myapp02'],
+                ],
+            ],
+        ]
+
+
+    def test_complex_query_parenthesis(self):
+        result = parser('myapp and source=syslog and (custom_field = "myapp01" or custom_field = "myapp02")')
+        assert result == ['AND',
+            ['SEARCH', 'myapp'],
+            ['AND',
+                ['=', 'source', 'syslog'],
+                ['OR',
+                    ['=', 'custom_field', 'myapp01'],
+                    ['=', 'custom_field', 'myapp02'],
+                ]
+            ],
+        ]
+
+class TestParserTypes:
+    def test_integer(self):
+        result = parser('123')
+        assert result == ['SEARCH', 123]
+
+    def test_negative_integer(self):
+        result = parser('-42')
+        assert result == ['SEARCH', -42]
+
+    def test_float(self):
+        result = parser('3.14')
+        assert result == ['SEARCH', 3.14]
+
+    def test_bool_true(self):
+        result = parser('mybool=true')
+        assert result == ['=', 'mybool', True]
+
+    def test_bool_false(self):
+        result = parser('mybool=false')
+        assert result == ['=', 'mybool', False]
+
+    def test_double_quoted_string(self):
+        result = parser('key = "value"')
+        assert result == ['=', 'key', 'value']
+
+    def test_single_quoted_string(self):
+        result = parser("key = 'value'")
+        assert result == ['=', 'key', 'value']
+
+    def test_double_quoted_escape(self):
+        result = parser(r'key = "value\t\n\"')
+        assert result == ['=', 'key', "value\t\n\\"]
+
+    def test_quoted_field(self):
+        result = parser('"myfield with space" = myapp01')
+        assert result == ['=', 'myfield with space', 'myapp01']
+
+    def test_single_quote_string(self):
+        result = parser("'myfield' = 'myvalue'")
+        assert result == ['=', 'myfield', 'myvalue']
+
+    def test_array(self):
+        result = parser('myfield = [1, 2, 3]')
+        assert result == ['=', 'myfield', [1, 2, 3]]
+
+    def test_nested_array(self):
+        result = parser('myfield = [[1], [2], [3]]')
+        assert result == ['=', 'myfield', [[1], [2], [3]]]
+
+    def test_dict(self):
+        result = parser('myfield = {a: 1, b: 2}')
+        assert result == ['=', 'myfield', {'a': 1, 'b': 2}]
+
+    def test_nested_dict(self):
+        result = parser('myfield = {a: {"mymessage": "x"}, b: 2}')
+        assert result == ['=', 'myfield', {'a': {'mymessage': 'x'}, 'b': 2}]
+
+class TestParserOperations:
+    def test_nequal(self):
+        result = parser('process != systemd')
+        assert result == ['!=', 'process', 'systemd']
+
+    def test_matches(self):
+        result = parser('message MATCHES "[aA]lert"')
+        assert result == ['MATCHES', 'message', '[aA]lert']
+
+    def test_matches_symbol(self):
+        result = parser('message ~ "[aA]lert"')
+        assert result == ['MATCHES', 'message', '[aA]lert']
+
+    def test_exists(self):
+        result = parser('custom_field EXISTS')
+        assert result == ['EXISTS', 'custom_field']
+
+    def test_exists_symbol(self):
+        result = parser('custom_field?')
+        assert result == ['EXISTS', 'custom_field']
+
+    def test_exists_symbol_not(self):
+        result = parser('!custom_field?')
+        assert result == ['NOT', ['EXISTS', 'custom_field']]
+
+    def test_gt(self):
+        result = parser('mail_queue>100')
+        assert result == ['>', 'mail_queue', 100]
+
+    def test_lt(self):
+        result = parser('port < 1024')
+        assert result == ['<', 'port', 1024]
+
+    def test_contains(self):
+        result = parser('rules contains myrule')
+        assert result == ['CONTAINS', 'rules', 'myrule']
+
+    def test_contains_array(self):
+        result = parser('myarray contains [1, 2, 3]')
+        assert result == ['CONTAINS', 'myarray', [1, 2, 3]]
+
+    def test_in(self):
+        result = parser('myrule in rules')
+        assert result == ['IN', 'myrule', 'rules']
+
+    def test_in_array(self):
+        result = parser('[1, 2, 3] in myarray')
+        assert result == ['IN', [1, 2, 3], 'myarray']
