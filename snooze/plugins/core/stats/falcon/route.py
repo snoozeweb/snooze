4c1c08cc9c3ccc7e6b8e7906bda1f0cfaaf0965d
Florian Dematraz <florian.dematraz@japannext.co.jp>
2021-10-25 18:30:08 +0900

    Adding Copyright

--- snooze/plugins/core/stats/falcon/route.py
+++ snooze/plugins/core/stats/falcon/route.py
@@ -1,3 +1,10 @@
+#
+# Copyright 2018-2020 Florian Dematraz <florian.dematraz@snoozeweb.net>
+# Copyright 2018-2020 Guillaume Ludinard <guillaume.ludi@gmail.com>
+# Copyright 2020-2021 Japannext Co., Ltd. <https://www.japannext.co.jp/>
+# SPDX-License-Identifier: AFL-3.0
+#
+
 #!/usr/bin/python
 
 import falcon
