ae64d6360c8239b1504bbd0e53ef98c2f403e6bb
Florian Dematraz <florian.dematraz@japannext.co.jp>
2022-01-21 17:28:05 +0900

    Conditions display issue

--- web/src/components/Condition.vue
+++ web/src/components/Condition.vue
@@ -1,14 +1,17 @@
 <template>
-  <span v-if="data === undefined || data == ''">Always true</span>
+  <span v-if="!rec && (data === undefined || data == '')">Always true</span>
   <span v-else-if="data.constructor.name == 'Array'">
-    <span v-if="data[0] == 'SEARCH'">
-      ( <b>{{ data[0] }}</b> <Condition :data="data[1]" /> )
+    <span v-if="!rec && data[0] == ''">
+      Always true
+    </span>
+    <span v-else-if="data[0] == 'SEARCH'">
+      ( <b>{{ data[0] }}</b> <Condition :data="data[1]" rec /> )
     </span>
     <span v-else-if="data[0] != 'NOT'">
-      ( <Condition :data="data[1]"/> <b>{{ data[0] }}</b> <Condition v-if="data[0] != 'EXISTS'" :data="data[2]" /> )
+      ( <Condition :data="data[1]" rec /> <b>{{ data[0] }}</b> <Condition v-if="data[0] != 'EXISTS'" :data="data[2]" rec /> )
     </span>
     <span v-else>
-      ( <b>{{ data[0] }}</b> <Condition :data="data[1]"/> )
+      ( <b>{{ data[0] }}</b> <Condition :data="data[1]" rec /> )
     </span>
   </span>
   <span v-else-if="data.constructor.name == 'String'">{{ data }}</span>
@@ -27,6 +30,7 @@ export default {
       // Array (it accepts String for recursion, but is not meant to be used like this)
       type: [Array, String],
     },
+    rec: {type: Boolean, default: () => false},
   },
 }
 </script>
