262950443d7827d0f744a22f81d49950a80a2c3e
Florian Dematraz <florian.dematraz@japannext.co.jp>
2022-05-16 11:28:07 +0900

    Updated all web packages + NodeJS (10->12)
    Vue to 3.2.33
    Coreui-vue to v4.3.0

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,8 +1,23 @@
+const path = require('path');
 module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
   publicPath: '/web',
   devServer: {
     port: 8082
-  }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('supportChaining')
+      .test(/\.js$/)
+        .include
+          .add(path.resolve('node_modules/@coreui'))
+          .end()
+      .use('babel-loader')
+        .loader('babel-loader')
+        .tap(options => ({ ...options,
+          plugins : ['@babel/plugin-proposal-optional-chaining']
+        }))
+        .end()
+    }
 }
